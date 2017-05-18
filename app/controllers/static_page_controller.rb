ceca822cedf13375e6b0c180deedf8af601cf2ae
gabba <gkuma@student.42.fr>
2017-05-18 23:37:57 +0100

    finish static pages

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,6 +2,9 @@ class StaticPageController < ApplicationController
   def home
   end
 
+  def about
+  end
+
   def help
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
+
   get 'static_page/home'
+
+  get 'static_page/about'
 
   get 'static_page/help'
 
-  root 'application#hello'
 end
